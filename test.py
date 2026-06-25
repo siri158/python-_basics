9cbf402a6d4c9064ba0c26fab1373e64260b0762
Ashmitha <ashmithashetty196@gmail.com>
2026-06-25 16:53:37 +0530

    git commands

---
+++ test.py
@@ -0,0 +1,38 @@
+print("hello worlrd",)
+print("hello world",end="")
+print("hello world",end="")
+
+print("hello world","world",sep=" #")
+
+
+
+
+x='10.5'
+x=int(float(x))
+print(type(x))
+
+print("\tesr\ntesting")
+print(r"\test\ntesting") 
+print("\\test\\ntesting")
+
+print("""hello
+      my name is
+      siri
+      """)
+
+x="hello"
+print(x[-1])
+
+
+
+
+for i in range(1,10):
+    if i%2==0:
+        print(i)
+
+""" while True:
+    print("hello world")     """  
+
+"""for i in range(0,10,2):
+    print(i)"""
+ 
